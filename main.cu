03d8e41ee2627cc5e856759c926951dae26fb65a
mihssk <m.babu2013@yandex.ru>
2025-11-25 04:55:00 +0300

    k_2 3 done k4 start

--- main.cu
+++ main.cu
@@ -7,9 +7,10 @@ using namespace std;
 
 
 #define BLOCK_SIZE 32
-#define THREAD_TILE_M 4
-#define THREAD_TILE_N 4
+#define BLOCK_SPLIT_N 4
 #define SYNC_T __syncthreads();
+
+
 void print_matrix(double* m, int m_size)
 {
     for(int i = 0; i < m_size; i++)
@@ -27,10 +28,12 @@ void check_matrix(const double* m1, const double* m2, int m_size, const string& 
 {
     bool ret = true;
     int p = 1;
+    double err = 0;
     for(int i = 0; i < m_size; i++)
     {
         for(int j = 0; j < m_size; j++)
         {
+            err += m1[i * m_size + j] - m2[i * m_size + j];
             if(m1[i * m_size + j] - m2[i * m_size + j] > precision)
             {
                 ret = false;
@@ -44,7 +47,7 @@ void check_matrix(const double* m1, const double* m2, int m_size, const string& 
     }
     if (ret)
     {
-        cout << "The matrices " << nm1 + " and " << nm2 << " are identical";
+        cout << "The matrices " << nm1 + " and " << nm2 << " are identical. Error: " <<  err;
     }
     cout << endl;
 }
@@ -162,15 +165,15 @@ __global__ void ker3_v2(const double* A, const double* B, double* C, int m_size)
     int col = threadIdx.x % BLOCK_SIZE;
     int row = threadIdx.x / BLOCK_SIZE;
 
-    const int cRow = blockIdx.x;
-    const int cCol = blockIdx.y;
+    int cRow = blockIdx.x;
+    int cCol = blockIdx.y;
 
     double tmp = 0.0f;
     if (row < m_size && col < m_size) {
         for (int i = 0; i < m_size / BLOCK_SIZE + (m_size % BLOCK_SIZE > 0); i++) {
             int A_col = i * BLOCK_SIZE + col;
             if (row < m_size && A_col < m_size) {
-                s_A[col + row * BLOCK_SIZE] = A[m_size * row + A_col];
+                s_A[col + row * BLOCK_SIZE] = A[m_size * (row + BLOCK_SIZE * cRow)  + A_col];
 
             } else {
                 s_A[col + row * BLOCK_SIZE] = 0;
@@ -178,7 +181,7 @@ __global__ void ker3_v2(const double* A, const double* B, double* C, int m_size)
 
             int B_row = i * BLOCK_SIZE + row;
             if (row < m_size && col < m_size) {
-                s_B[col + BLOCK_SIZE * row] = B[m_size * B_row + col];
+                s_B[col + BLOCK_SIZE * row] = B[m_size * B_row + (col + BLOCK_SIZE * cCol)];
 
             } else {
                 s_B[col + BLOCK_SIZE * row] = 0;
@@ -186,20 +189,42 @@ __global__ void ker3_v2(const double* A, const double* B, double* C, int m_size)
             SYNC_T
 
             for (int j = 0; j < BLOCK_SIZE; j++) {
-                tmp += s_A[j][threadIdx.y] * s_B[threadIdx.x][j];
+                tmp += s_A[j + row * BLOCK_SIZE] * s_B[col + j * BLOCK_SIZE];
             }
             SYNC_T
 
         }
-        C[row * m_size + col] = tmp;
+        C[(row + BLOCK_SIZE * cRow) * m_size + (col + BLOCK_SIZE * cCol)] = tmp;
     }
 }
+__global__ void ker4(const double* A, const double* B, double* C, int m_size)
+{
+    __shared__ double s_A[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ double s_B[BLOCK_SIZE][BLOCK_SIZE];
 
+    float reg_C[BLOCK_SPLIT_N][BLOCK_SPLIT_N] = {0.0f};
+    float reg_A[BLOCK_SPLIT_N];
+    float reg_B[BLOCK_SPLIT_N];
+
+    int b_col = blockIdx.x * BLOCK_SIZE + threadIdx.x * BLOCK_SPLIT_N;
+    int b_row = blockIdx.y * BLOCK_SIZE + threadIdx.y * BLOCK_SPLIT_N;
+    for(int tile_i = 0; tile_i < m_size / BLOCK_SIZE + (m_size % BLOCK_SIZE > 0); tile_i++)
+    {
+        for(int split_x = 0; split_x < BLOCK_SPLIT_N; split_x++)
+        {
+            for(int split_y = 0; split_y < BLOCK_SPLIT_N; split_y++)
+            {
+                s_A[threadIdx.x * BLOCK_SPLIT_N + split_x][threadIdx.y * BLOCK_SPLIT_N + split_y] = A[b_col + split_x + (b_row + split_y) * m_size]
+            }
+        }
+
+    }
+}
 
 int main() {
 
 
-    int N = 128;
+    int N = 1024;
     auto* h_A = new double[N * N];
     auto* h_B = new double[N * N];
     auto* h_C = new double[N * N];
@@ -237,7 +262,7 @@ int main() {
     }
     auto end = chrono::high_resolution_clock::now();
     double time_elapsed = (double)chrono::duration_cast<chrono::microseconds >(end - start).count();
-    cout << "CPU time: "<< time_elapsed << endl << endl;
+    cout << "CPU time: "<< time_elapsed / 1000000 << endl << endl;
 
     start = chrono::high_resolution_clock::now();
     {
@@ -292,7 +317,32 @@ int main() {
     cout << "ker3 time: " << time_elapsed / 1000000 << endl << endl;
     check_matrix(d_C_res, h_C, N, "cpu", "ker3");
 
+    start = chrono::high_resolution_clock::now();
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            ker3_v2<<<grid_size, BLOCK_SIZE * BLOCK_SIZE>>>(d_A, d_B, d_C, N);
+        }
+        cudaMemcpy(d_C_res, d_C, sizeof(double) * N * N, cudaMemcpyDeviceToHost);
 
+//        print_matrix(d_C_res, N);
+    }
+
+    start = chrono::high_resolution_clock::now();
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            ker4<<<grid_size, dim3(BLOCK_SIZE / BLOCK_SPLIT_N, BLOCK_SIZE / BLOCK_SPLIT_N)>>>(d_A, d_B, d_C, N);
+        }
+        cudaMemcpy(d_C_res, d_C, sizeof(double) * N * N, cudaMemcpyDeviceToHost);
+
+//        print_matrix(d_C_res, N);
+    }
+
+    end = chrono::high_resolution_clock::now();
+    time_elapsed = (double)chrono::duration_cast<chrono::microseconds >(end - start).count();
+    cout << "ker3_v2 time: " << time_elapsed / 1000000 << endl << endl;
+    check_matrix(d_C_res, h_C, N, "cpu", "ker3_v2");
 
     delete[] h_A;
     delete[] h_B;
